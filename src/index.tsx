df8e26cd72dbd993d57d1f3925b9efc32a60baf8
sixu <277841643@qq.com>
2022-03-10 22:55:35 +0800

    header and top

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+moment.locale('zh-cn');
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +14,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
