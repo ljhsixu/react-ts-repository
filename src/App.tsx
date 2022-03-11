7fa0177051290b5136e12f9daf6d1b74a2629059
sixu <277841643@qq.com>
2022-03-11 22:23:39 +0800

    feat: '页面布局完成'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styles from './App.module.css'
-import { Header, Footer,Carousel,SideMenu } from './components'
-import { Row, Col } from 'antd'
+import { Header, Footer, Carousel, SideMenu,ProductColletion } from './components'
+import { Row, Col, Typography } from 'antd'
+import { productList1,productList2,productList3 } from './mockups'
+import sideImage from './assets/images/sider_2019_12-09.png'
+import sideImage2 from './assets/images/sider_2019_02-04.png'
+import sideImage3 from './assets/images/sider_2019_02-04-2.png'
+
 
 function App() {
   return (
@@ -12,11 +17,26 @@ function App() {
         <Row style={{ marginTop: 20 }}>
           <Col span={6}>
             <SideMenu></SideMenu>
-            </Col>
+          </Col>
           <Col span={18}>
-           <Carousel></Carousel> 
+            <Carousel></Carousel>
           </Col>
         </Row>
+        <ProductColletion
+        title={<Typography.Title level={3} type='warning'>爆款推荐</Typography.Title>}
+        sideImage={sideImage}
+        products={productList1}
+        ></ProductColletion>
+         <ProductColletion
+        title={<Typography.Title level={3} type='warning'>新品上市</Typography.Title>}
+        sideImage={sideImage2}
+        products={productList2}
+        ></ProductColletion>
+         <ProductColletion
+        title={<Typography.Title level={3} type='warning'>国内游推荐</Typography.Title>}
+        sideImage={sideImage3}
+        products={productList3}
+        ></ProductColletion>
       </div>
       <Footer></Footer>
     </div>
